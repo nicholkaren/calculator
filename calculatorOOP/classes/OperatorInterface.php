efe1e2f71fd8872875614ba98316adfb92900b30
Nichol Olguin <nichololguin89@gmail.com>
2017-12-04 18:44:36 +0100

    calculator oop
    Ny mapp, förvirrande då det finns liknande namn. Kanske funkar kanske inte?

---
+++ calculatorOOP/classes/OperatorInterface.php
@@ -0,0 +1,7 @@
+<?php
+
+interface OperatorInterface
+{
+    public function run($number, $result);
+    
+}
---
+++ calculatorOOP/index.php
@@ -0,0 +1,29 @@
+<?php
+require 'classes/Calculator1.php';
+require 'classes/OperatorInterface.php';
+require 'classes/Adder.php';
+require 'classes/Subtractor.php';
+
+ $num1 = $_POST['number1'];
+ $num2 = $_POST['number2'];
+
+$c = new Calculator;
+
+$c->setOperation(new Adder);
+$c->calculate($num1, $num2);
+
+echo $c->getResult();
+
+?>
+
+ <form method="post" action="index.php">
+		        <input name="number1" type="text" class="form-control" style="width: 150px; display: inline" />
+		        <select name="operation">
+		        	<option value="plus">Plus</option>
+		            <option value="minus">Minus</option>
+		            <option value="times">Times</option>
+		            <option value="divided by">Divided By</option>
+		        </select>
+		        <input name="number2" type="text" class="form-control" style="width: 150px; display: inline" />
+		        <input name="submit" type="submit" value="Calculate" class="btn btn-primary" />
+		    </form>
