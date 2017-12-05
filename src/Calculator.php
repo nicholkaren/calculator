f4590dd8b4f63db88922801a3dca3f71d85232a3
Katerina <katerinanikou@gmail.com>
2017-12-06 00:45:11 +0100

    Addition, subtraction, division is working
    En stor kommit där vi dels lagt till htmlkod så vi kan se vad vi gör, samt gjort det möjligt att välja operator och utföra olika uräkningar. Många kommentarer i koden för vidare utveckling!
    
    Det som funkar nu är
    Plus
    Minus
    Delat med (Dock måste Test ändras)

--- src/Calculator.php
+++ src/Calculator.php
@@ -2,37 +2,59 @@
 
 class Calculator
 {
-    
+
     private $sum = 0; 
     
     public function getSum()
     {
         return $this->sum;
     } 
-    
+
+
     public function addition(array $values)
     {        
+
         foreach ($values as $value) {
             $this->sum += $value;
         }
         
         return $this->sum;
     }
+
+
+    // OBS!!! Ny funktion som testar enbart 2 fält!!
+    //public function simpleAddition ($numOne, $numTwo)
+   // {
+   //     return $numOne + $numTwo;
+   // }
     
-    public function division($value) 
+    public function division($values) 
     {
-        if($value ==0){
-            throw new \InvalidArgumentException;    
-        }
-        $this->sum = $this->sum / $value;
+        // i arrayen values så finns 2 tal. [0] är första talet. [1] är andra talet.
+        $sum = $values[0];
+
+        return $sum / $values[1];
+
+        //if($values == 0){
+          //  throw new \InvalidArgumentException;    
+       // }
+        //$this->sum = $this->sum / $value;
+
+        // Kolla numOne/numTwo
     }
     
+
     public function subtraction(array $values)
     {
-        foreach ($values as $value)
-        {
-            $this->sum -=$value;
-        }
+        // i arrayen values så finns 2 tal. [0] är första talet. [1] är andra talet.
+        $sum = $values[0];
+
+        return $sum - $values[1];
+
+        //foreach ($values as $value)
+        //{
+          //  $this->sum -=$value;
+        //}
         
         return $this->sum;
     }
@@ -58,6 +80,83 @@ class Calculator
     }
 
 
-
     }
-?>
+        // 6/12-2017
+        // Försök lägga in $numOne och $numTwo i en array ($values) som vi använder i
+        // funktionerna. Var_dump och die(); hela tiden för att se så att vi verkligen
+        // får in summorna i arrayen. Slutresultat ska bli: 
+        // $value = [] -> var_dump($value) -> numOne = 1, numTwo = 3.
+
+        // Går det inte så skriv om funktionerna som i exemplet. 
+
+        
+        // Deklarerar variabler. $_POST är en array. För att få ut siffran på key = numOne -> $_POST['numOne']
+        $numOne = $_POST['numOne'];
+        $numTwo = $_POST['numTwo'];
+
+
+        $values = array();
+
+        array_push($values, $numOne, $numTwo);
+
+    
+        // Nyar upp en instans av klassen calculator för att kunna kalla på funktionerna som vi har skapat i
+        // klassen. 
+        $cal = new calculator();
+
+
+        // Här kollar vi nu på key (name i select i form) i $_POST på vilken operation som ska köras. 
+        // Dina options i selectmenyn har ett varsitt value, så vi kan kolla om det överensstämmer med
+        // ['operation']. Om $_POST innehåller addition så körs funktionen addition. Och det gör man genom
+        // att kalla på den via $cal->addition(); och $cal är den nya instansen/versionen av calculatorn.
+
+        if($_POST['operation']=="addition")
+        {   
+            $result = $cal->addition($values);
+        } 
+
+        if($_POST['operation']=="subtraction")
+        {
+           $result = $cal->subtraction($values);
+        } 
+
+        if($_POST['operation']=="division")
+        {
+           $result = $cal->division($values);
+        } 
+
+        if($_POST['operation']=="multiplication")
+        {
+           $result = $cal->modulus($values);
+        } 
+
+        if($_POST['operation']=="modulus")
+        {
+           $result = $cal->modulus($values);
+        } 
+
+       
+
+   ?>  
+
+ <!-- Form med action till den fil där man har funktionen man vill köra. I detta
+ fall blir det samma fil. Metoden POST/post betyder att man skickar med values till
+funktionen. De sparas i en variabel som heter $_POST som är en array. Den kan man 
+alltid var_dump($_POST) om man vill veta vad man har skickat med i formuläret. 
+
+name= key i arrayen. -->     
+<form name="Calculator" action="Calculator.php" method="POST">
+    <input type="text" name="numOne" value="<?php echo $numOne;?>">
+    <select name="operation">
+                    <option value="addition">Plus</option>
+                    <option value="subtraction">Minus</option>
+                    <option value="multiplication">Gånger</option>
+                    <option value="division">Delat med</option>
+                    <option value="modulus">Modulus</option>
+                </select>
+    <input type="text" name="numTwo" value="<?php echo $numTwo;?>">
+
+    <button type="submit" class=""> = </button> <?php echo $result; ?> <br>
+
+</form>
+
