d7ac6a2041db185d9596d33728ff69851bec0f8d
Katerina <katerinanikou@gmail.com>
2017-12-06 23:33:29 +0100

    Last push...
    Tog bort OOP-mappen

--- calculatorOOP/classes/Adder.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Adder implements OperatorInterface
-{
-    public function run ($number, $result)
-    {
-        return $result + $number;
-    }
-}
--- calculatorOOP/classes/OperatorInterface.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-interface OperatorInterface
-{
-    public function run($number, $result);
-    
-}
